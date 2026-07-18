8b1d62d9b2d09062e7f07084fe4240e7b5f6b2d8
minorin2847 <minorin2847@gmail.com>
2026-07-18 22:40:42 +0700

    chore: add production railway host to vite dev server configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envDir: '../',
+  server: {
+    allowedHosts: ["frontend-production-cb3f5.up.railway.app"],
+  }
 })
